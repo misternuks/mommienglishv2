8c3b550518b0ad64730dbf354c03c941889dce88
Matthew Miner <misternuks@gmail.com>
2025-05-06 14:37:36 +0900

    create and style Header component; import in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="content-center justify-center h-screen">
-      <div className="text-center">
-        <h1 className="font-shippori-mincho-b1 text-6xl text-shadow-[4px_5px_7px_rgba(0,0,0,0.25)]">あなたの英語習慣を<br/>
-          デザインします
-        </h1>
-        <p className="font-zen-maru-gothic text-4xl">お問い合わせはこちら</p>
+    <main>
+      <div className="content-center justify-center h-screen">
+        <div className="text-center">
+          <h1 className="font-shippori-mincho-b1 text-6xl text-shadow-[4px_5px_7px_rgba(0,0,0,0.25)]">あなたの英語習慣を<br/>
+            デザインします
+          </h1>
+          <p className="font-zen-maru-gothic text-4xl">お問い合わせはこちら</p>
+        </div>
       </div>
-    </div>
+      <Header />
+    </main>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="w-full bg-white text-brand-text p-5 flex justify-center items-center">
+      <nav className="w-5xl space-x-4 flex justify-around items-center">
+        <a href="#">Home</a>
+        <a href="#">Our mission</a>
+        <a href="#">Service</a>
+        <a href="#">Voice</a>
+        <a href="#">About me</a>
+      </nav>
+    </header>
+  );
+}
