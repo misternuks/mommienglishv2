9f6fc4455e658e23f4decd82a609c4ae79977d24
Matthew Miner <misternuks@gmail.com>
2025-05-07 23:06:19 +0900

    create Service component; add content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Mission from "@/components/Mission";
+import Service from "@/components/Service";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Header />
       <Mission />
+      <Service />
     </main>
   );
 }
